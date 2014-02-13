f5b00cb7b77b2955ce36492941883a5dedf929e2
WonJae Yang <WonJaeYang@101-24-41-155-wireles1x.bu.edu>
2014-02-13 00:06:45 -0500

    Initial Commit

---
+++ happy/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  happy
+//
+//  Created by WonJae Yang on 2/13/14.
+//  Copyright (c) 2014 WonJae Yang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
